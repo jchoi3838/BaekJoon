e87655bb59866c2ab5d14c46f8b938a6ef87e46c
Joonyeong Choi / 최준영 <jchoi3838@icloud.com>
2021-04-16 04:16:35 +0900

    Level_3

---
+++ Level_3.java
@@ -0,0 +1,8 @@
+public class Level_3 {
+	public static void main(String[] args) {
+		System.out.println("\\      /\\");
+		System.out.println(" )    ( ')");
+		System.out.println("(    /  )");
+		System.out.println("  \\ (__)|");
+	}
+}
