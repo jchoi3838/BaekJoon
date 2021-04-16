0a4cbd65313e0e02b9ac4171cef0f39c544a24ea
Joonyeong Choi / 최준영 <jchoi3838@icloud.com>
2021-04-16 19:26:13 +0900

    Update Level_14.java

--- Level_14.java
+++ Level_14.java
@@ -3,8 +3,23 @@ import java.util.Scanner;
 public class Level_14 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.println(a - b);
+		int num = sc.nextInt();
+		int[] arr = new int[num];
+
+		for(int i = 0; i < num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int min = arr[0];
+		int max = arr[0];
+		
+		for(int i = 0; i < arr.length; i++) {	
+			if(min > arr[i])
+				min = arr[i];
+			if(max < arr[i])
+				max = arr[i];
+		}
+		System.out.println(min + " " + max);
 	}
+	
 }
