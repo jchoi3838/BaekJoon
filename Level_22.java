d2741914a80a1e89547fabf64312af7ba2b9d30b
Joonyeong Choi / 최준영 <jchoi3838@icloud.com>
2021-04-19 20:44:12 +0900

    Create Level_22.java

---
+++ Level_22.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Level_22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			for(int j = i+1; j < n; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k < i+1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
