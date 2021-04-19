1159a85a401c899b161ab1fc0fe0772c5585afb1
Joonyeong Choi / 최준영 <jchoi3838@icloud.com>
2021-04-19 21:31:07 +0900

    Update Level_25.java

--- Level_25.java
+++ Level_25.java
@@ -5,50 +5,13 @@ public class Level_25 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int[] arr = new int[10];
-		int result = a * b * c;
-		while (result != 0) {
-			int temp = result % 10;
-			result /= 10;
-			switch (temp) {
-			case 0:
-				arr[0]++;
-				break;
-			case 1:
-				arr[1]++;
-				break;
-			case 2:
-				arr[2]++;
-				break;
-			case 3:
-				arr[3]++;
-				break;
-			case 4:
-				arr[4]++;
-				break;
-			case 5:
-				arr[5]++;
-				break;
-			case 6:
-				arr[6]++;
-				break;
-			case 7:
-				arr[7]++;
-				break;
-			case 8:
-				arr[8]++;
-				break;
-			case 9:
-				arr[9]++;
-				break;
-
-			}
+		int n = sc.nextInt();
+		int result = 0;
+		
+		for(int i = 1; i<= n; i++) {
+			result += i;
 		}
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
+		
+		System.out.println(result);
 	}
 }
