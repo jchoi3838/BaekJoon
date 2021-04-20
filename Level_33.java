af404a7fa294290f68d1d1dc40bccea584b5d4fb
Joonyeong Choi / 최준영 <jchoi3838@icloud.com>
2021-04-21 07:35:47 +0900

    Create Level_33.java

---
+++ Level_33.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Level_33 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = N; i >= 1; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
