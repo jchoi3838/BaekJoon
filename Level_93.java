ec9c41c6759702a6f8f9672f1a00b8eceff35057
Joonyeong Choi / 최준영 <jchoi3838@icloud.com>
2021-06-08 19:21:03 +0900

    Create Level_93.java

---
+++ Level_93.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Level_93 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n + 1];
+		int[] dp = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for (int i = 1; i <= n; i++) {
+			dp[i] = 1;
+			for (int j = 1; j < i; j++) {
+				if (arr[i] > arr[j] && dp[i] <= dp[j])
+					dp[i] = dp[j] + 1;
+			}
+		}
+		Arrays.sort(dp);
+		System.out.println(n - dp[n]);
+	}
+}
